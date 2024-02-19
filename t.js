f0e4a6ebae28987e296817cd2af312da2239999b
codeyumm <pvashi006@gmail.com>
2024-02-18 19:19:17 -0500

    added js to get p;ayer data

---
+++ t.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+
+var ids = [{"Id":35320},
+{"Id":50710},
+{"Id":253802},
+{"Id":7133},
+{"Id":49209},
+{"Id":49289},
+{"Id":40570},
+];
+
